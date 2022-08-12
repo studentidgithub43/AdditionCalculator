574629968229e3eb4083b41b4bbedf490ab8e7c7
studentidgithub43 <111099541+studentidgithub43@users.noreply.github.com>
2022-08-11 18:44:24 -0700

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,14 +1,14 @@
 class Program{
-static void Main(string[] args){
-Console.WriteLine("Addition Calculator");
+  static void Main(string[] args){
+    Console.WriteLine("Addition Calculator");
 
-Console.Write("Enter the first number: ");
-int a = Convert.Int32(Console.ReadLine());
-Console.Write("Enter the second number: ");
-int b = Convert.Int32(Console.ReadLine());
-int result = a + b;
+    Console.Write("Enter the first number: ");
+    int a = Convert.Int32(Console.ReadLine());
+    Console.Write("Enter the second number: ");
+    int b = Convert.Int32(Console.ReadLine());
+    int result = a + b;
 
-Console.WriteLine("The total is: " + result);
-Console.Read();
-}
+    Console.WriteLine("The total is: " + result);
+    Console.Read();
+  }
 }
